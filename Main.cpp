d4a29433783ead0a642421d5796bd5efd6a20a52
Evan <ehanz1997@gmail.com>
2020-12-20 23:56:25 -0500

    added min

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 
+//returns the higher value
 template <typename T>
-
 T myMax (T x, T y)
 {
     return (x > y) ? x : y;
+}
+
+//returns the lower value
+template <typename T>
+T myMin (T x, T y)
+{
+    return (x < y) ? x : y;
 }
 
 int main() 
@@ -19,6 +26,12 @@ int main()
   
     // call myMax for char 
     cout << myMax <char> ('g', 'e') << endl; 
+
+    //call myMin for float
+    cout << myMin <float> (2.0, 4.0) << endl;
+
+    //call myMin for int
+    cout << myMin <int> (8, 3) << endl;
   
     return 0; 
 } 
